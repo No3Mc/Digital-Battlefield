058b73f837c652be4d043931b726d8b9735925cb
No3Mc <41834061+No3Mc@users.noreply.github.com>
2021-12-30 01:19:20 +0000

    Enemies/bot1:

--- Enemies/bot1.js
+++ Enemies/bot1.js
@@ -1,20 +1,20 @@
 //create the constructor for the class square
-function Star() {
+function bot1() {
     //initialisation code will go here
 
     //create private variables for the x and y coordinates
     this.x = Math.random() * 400;
     this.y = Math.random() * 200;
-    //set the size of the star
+    //set the size of the bot1
     var size = 2;
 
-    Star.prototype.draw = function (context) {
+    bot1.prototype.draw = function (context) {
         //save the state of the drawing context before we change it
         context.save();
         context.fillStyle = "#FFFFFF";
         //set the coordinates of the drawing area of the new shape to x and y
         context.translate(this.x, this.y);
-        //start the line (path)
+        //bot1t the line (path)
         context.beginPath();
         context.moveTo(-size, size);
         context.lineTo(size, size);
@@ -31,10 +31,10 @@ function Star() {
         context.restore();
     }
 
-    Star.prototype.move = function () {
+    bot1.prototype.move = function () {
         //change the x axis
         this.x -= .1;
-        //if the star has drifted off the left of the canvas
+        //if the bot1 has drifted off the left of the canvas
         if (this.x < 0) {
             //place it at the right of the canvas
             this.x = 400;
